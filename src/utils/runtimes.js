1ce83d921eb4d34dde3b51007fca8ee9be6ac0b1
kemcake <santos.remi@icloud.com>
2017-08-02 10:16:42 -0700

    Add dependency file editor

--- src/utils/runtimes.js
+++ src/utils/runtimes.js
@@ -24,19 +24,22 @@ export default [
   { value: 'nodejs6.10',
     label: 'NodeJS (6.10)',
     language: 'javascript',
-    supportDeps: false,
+    supportDeps: true,
+    depsFilename: 'package.json',
     defaultFunction: nodeFunction
   },
   { value: 'ruby2.4',
     label: 'Ruby (2.4)',
     language: 'ruby',
-    supportDeps: false,
+    supportDeps: true,
+    depsFilename: 'Gemfile',
     defaultFunction: rubyFunction
   },
   { value: 'python27',
     label: 'Python (27)',
     language: 'python',
     supportDeps: true,
+    depsFilename: 'requirements.txt',
     defaultFunction: pythonFunction
   }
 ]
